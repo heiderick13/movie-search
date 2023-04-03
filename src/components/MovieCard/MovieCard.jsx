ce7db32cb06e07320c77ae0e564da7ca4e9781fb
Marcelo Oliveira <marceloheiderick@outlook.com>
2023-04-03 17:15:04 -0300

    :lipstick: style movie card

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -5,8 +5,14 @@ const imgURL = import.meta.env.VITE_IMG;
 function MovieCard({ movie }) {
   return (
     <div className="movie-card">
-      <img src={imgURL + movie.poster_path} alt={movie.title} />
+      <img
+        className="movie-img"
+        src={imgURL + movie.poster_path}
+        alt={movie.title}
+      />
       <h3 className="movie-title">{movie.title}</h3>
+      <p className="movie-popularity">{movie.popularity.toFixed(2)}</p>
+      <p className="movie-date">{movie.release_date}</p>
     </div>
   );
 }
